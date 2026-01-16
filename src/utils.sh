857641c25fd962cd83b6b321a3e065d0e982042b
mkonig <koenig@v-i-t.de>
2026-01-16 23:39:45 +0100

    feat: Better log output handling.

--- src/utils.sh
+++ src/utils.sh
@@ -11,25 +11,47 @@
 # Or move the check to the logging function itself to avoid evaluating
 # expensive command substitutions in the caller
 
+log_level_info=0
+log_level_error=1
+log_level_warning=2
+log_level_debug=3
+
+log_level=$log_level_debug
+
 # shellcheck disable=SC2154
 logging() {
     local level=$1
     local message=$2
-    gum log -o whatisleft.log -t rfc822 -l "${level}" "${message}"
+
+    if (( level > log_level )) ; then
+        return
+    fi
+
+    case $level in
+        "$log_level_info")
+            level_text="info" ;;
+        "$log_level_debug")
+            level_text="debug" ;;
+        "$log_level_warning")
+            level_text="warning";;
+        "$log_level_error")
+            level_text="error";;
+    esac
+    gum log -o whatisleft.log -t rfc822 -l "${level_text}" "${message}"
 }
 
 log_debug() {
-    logging "debug" "${1}"
+    logging $log_level_debug "${1}"
 }
 
 log_error() {
-    logging "error" "${1}"
+    logging $log_level_error "${1}"
 }
 
 log_warning() {
-    logging "warn" "${1}"
+    logging $log_level_warning "${1}"
 }
 
 log_info(){
-    logging "info" "${1}"
+    logging $log_level_info "${1}"
 }
